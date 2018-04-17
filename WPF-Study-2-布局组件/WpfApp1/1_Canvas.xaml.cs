7f6452a98a6fcc403bb427dd192e13c6ed99207e
刘晨 <7578916585@qq.com>
2018-04-17 17:47:42 +0800

    WPF第二课的学习

---
+++ WPF-Study-2-布局组件/WpfApp1/1_Canvas.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// _1_Canvas.xaml 的交互逻辑
+    /// </summary>
+    public partial class _1_Canvas : Window
+    {
+        public _1_Canvas()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPF-Study-2-布局组件/WpfApp1/5_Grid.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// _5_Grid.xaml 的交互逻辑
+    /// </summary>
+    public partial class _5_Grid : Window
+    {
+        public _5_Grid()
+        {
+            InitializeComponent();
+        }
+    }
+}
